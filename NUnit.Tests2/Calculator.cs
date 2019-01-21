27a4f760f5d868239e7736b13496290fcb3be4d4
ArtemPervushow <drperv@yandex.ru>
2019-01-21 10:57:11 +0300

    Add files via upload

---
+++ NUnit.Tests2/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests2
+{
+    public class Calculator
+    {
+        public static int plus(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ NUnit.Tests2/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests2
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Assert.Pass("Your first passing test");
+            Assert.AreEqual(4, Calculator.plus(2,2));
+        }
+    }
+}
